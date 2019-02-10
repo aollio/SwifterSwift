4ac9b1c1c2d66cbf0a7d7a3701302bc60cab92ab
Karainon <imkarainon@gmail.com>
2019-02-10 17:38:43 +0800

    Sources/Extensions/UIKit/UIRefreshControlExtensions.swift

--- Sources/Extensions/UIKit/UIRefreshControlExtensions.swift
+++ Sources/Extensions/UIKit/UIRefreshControlExtensions.swift
@@ -18,13 +18,13 @@ public extension UIRefreshControl {
     ///   - tableView: UITableView instance, inside which the refresh control is contained.
     ///   - animated: Boolean, indicates that is the content offset changing should be animated or not.
     ///   - sendAction: Boolean, indicates that should it fire sendActions method for valueChanged UIControlEvents
-    public func beginRefreshing(in tableView: UITableView, animated: Bool, sendAction: Bool = false) {
+    public func beginRefreshing(in collectionView: UITableView, animated: Bool, sendAction: Bool = false) {
         //https://stackoverflow.com/questions/14718850/uirefreshcontrol-beginrefreshing-not-working-when-uitableviewcontroller-is-ins/14719658#14719658
-        assert(superview == tableView, "Refresh control does not belong to the receiving table view")
+        assert(superview == collectionView, "Refresh control does not belong to the receiving table view")
 
         beginRefreshing()
         let offsetPoint = CGPoint(x: 0, y: -frame.height)
-        tableView.setContentOffset(offsetPoint, animated: animated)
+        collectionView.setContentOffset(offsetPoint, animated: animated)
 
         if sendAction {
             sendActions(for: .valueChanged)
