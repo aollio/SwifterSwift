74481c58204a8b535d38080ca88110b1bfb8449a
Yudong <imkarainon@gmail.com>
2019-04-29 13:52:55 +0800

    Auto commit at 2019-04-29T13:52:55.835722

--- Sources/Extensions/UIKit/UIViewControllerExtensions.swift
+++ Sources/Extensions/UIKit/UIViewControllerExtensions.swift
@@ -12,11 +12,11 @@ import UIKit
 // MARK: - Properties
 public extension UIViewController {
 
-    /// SwifterSwift: Check if ViewController is onscreen and not hidden.
-    public var isVisible: Bool {
-        // http://stackoverflow.com/questions/2777438/how-to-tell-if-uiviewcontrollers-view-is-visible
-        return isViewLoaded && view.window != nil
-    }
+//    /// SwifterSwift: Check if ViewController is onscreen and not hidden.
+//    public var isVisible: Bool {
+//        // http://stackoverflow.com/questions/2777438/how-to-tell-if-uiviewcontrollers-view-is-visible
+//        return isViewLoaded && view.window != nil
+//    }
 
 }
 
